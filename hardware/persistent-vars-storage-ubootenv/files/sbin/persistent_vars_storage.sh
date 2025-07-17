045ab1849a88d73eae8801c174f07d39ad7d30b2
Prabha Murugesan <prabha.murugesan@morsemicro.com>
2025-07-17 23:13:49 +0000

    boards: (APP-4363, PR #165) Align ekh19 board name
    Approved-by: Sophronia Koilpillai
    Approved-by: James Haggerty

--- hardware/persistent-vars-storage-ubootenv/files/sbin/persistent_vars_storage.sh
+++ hardware/persistent-vars-storage-ubootenv/files/sbin/persistent_vars_storage.sh
@@ -65,7 +65,8 @@ show_factory_data()
 				;;
 			esac
 		;;
-		glinet,gl-mt3000)
+		glinet,gl-mt3000|\
+		morse,*ekh19*)
 			case "$1" in
 				default_wifi_key)
 					show_raw_partition_data Factory 0x40 16
