77fef05828266b0972ac29a1b8d1df1b7016ea7c
James Haggerty <james.haggerty@morsemicro.com>
2025-10-15 00:28:48 +0000

    morse-regdb: (APP-5357, PR #246) correct channelizations column name
    Since there can be multiple, it should be channelizations.
    
    
    Approved-by: Sophronia Koilpillai
    Approved-by: Lyall Beveridge

--- essentials/morse-regdb/fix_channelization_info.py
+++ essentials/morse-regdb/fix_channelization_info.py
@@ -7,25 +7,25 @@ import sys
 
 dr = csv.DictReader(sys.stdin)
 fieldnames = list(dr.fieldnames)
-fieldnames.insert(1, 'channelization')
+fieldnames.insert(1, 'channelizations')
 fieldnames.insert(2, 'default_channelization')
 
 dw = csv.DictWriter(sys.stdout, fieldnames, lineterminator='\n')
 dw.writeheader()
 for row in dr:
     if row['country'] == 'Australia':
-        row['channelization'] = '80211_2024;80211_revmf'
+        row['channelizations'] = '80211_2024;80211_revmf'
         row['default_channelization'] = True
     elif row['country'] == 'Australia Proposed':
         row['country'] = 'Australia'
-        row['channelization'] = '80211_revmf'
+        row['channelizations'] = '80211_revmf'
         row['default_channelization'] = True
     elif row['country'] == 'Australia 2020':
         row['country'] = 'Australia'
-        row['channelization'] = '80211_2020'
+        row['channelizations'] = '80211_2020'
         row['default_channelization'] = False
     else:
-        row['channelization'] = ''
+        row['channelizations'] = ''
         row['default_channelization'] = True
 
     dw.writerow(row)
