54cadae1efe1672b1a0e1a270393dc134646443b
Lyall Beveridge <lyall.beveridge@morsemicro.com>
2025-11-20 22:13:53 +0000

    luci-app-\*guide\*: (APP-5643, PR #303) update guides for 2.11.4
    Approved-by: James Haggerty
    Approved-by: Prabha Murugesan

--- luci/luci-app-morseguide-link/htdocs/luci-static/resources/view/morseguide/morseguide.js
+++ luci/luci-app-morseguide-link/htdocs/luci-static/resources/view/morseguide/morseguide.js
@@ -3,7 +3,7 @@
 'require view';
 
 // WARNING: Every time the offline user guide is updated, this link must be updated as well.
-const S3_BUCKET_USER_GUIDE_URL = 'https://repo.apps.morsemicro.com/openwrt/resources/UG+MM6108_MM8108+Eval+Kit+User+Guide+2.11.2.pdf';
+const S3_BUCKET_USER_GUIDE_URL = 'https://repo.apps.morsemicro.com/openwrt/resources/UG+MM6108_MM8108+Eval+Kit+User+Guide+2.11+-+v27.pdf';
 
 return view.extend({
 	handleSaveApply: null,
