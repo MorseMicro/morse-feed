55f530a8cc2b053973d23653e770bf025b2233fd
Lyall Beveridge <lyall.beveridge@morsemicro.com>
2026-03-19 02:58:47 +0000

    morse-bundle: (APP-6769, PR #388) allow SSH access when authorized key is present in flash
    Approved-by: James Haggerty
    Approved-by: Prabha Murugesan

--- hardware/morse-bundle/files/morse/scripts/save_dropbear_setup.sh
+++ hardware/morse-bundle/files/morse/scripts/save_dropbear_setup.sh
@@ -3,13 +3,8 @@
 # If you use this script to persist your dropbear setup, it will
 # get automatically loaded on a fresh flash (i.e. not retaining settings).
 
-if [ -e /etc/dropbear/authorized_keys ]; then 
+if [ -s /etc/dropbear/authorized_keys ]; then
 	persistent_vars_storage.sh WRITE dropbear_authorized_keys "$(cat /etc/dropbear/authorized_keys)"
+else
+	persistent_vars_storage.sh ERASE dropbear_authorized_keys
 fi
-
-for f in /etc/dropbear/dropbear_ed25519_host_key /etc/dropbear/dropbear_rsa_host_key; do
-	if [ -e "$f" ]; then
-		persistent_vars_storage.sh WRITE "$(basename "$f")" "$(base64 "$f")"
-	fi
-done
-
